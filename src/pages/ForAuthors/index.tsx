facdcfbec8e2b2369617a9173dfaf8c813b2cd95
Himalaya Senapati <himalayasenapati@Himalayas-MacBook-Air.local>
2025-11-24 02:28:21 +0530

    cleaned up the for author page

--- src/pages/ForAuthors/index.tsx
+++ src/pages/ForAuthors/index.tsx
@@ -2,32 +2,16 @@ import React from "react";
 import "./forauthors.css";
 import { useTranslation } from "react-i18next";
 
-const latexExample = `% Compile with: xelatex
-\\documentclass[12pt]{article}
-\\usepackage{fontspec}
-\\usepackage{polyglossia}
-\\setmainlanguage{english}
-\\setotherlanguage{oriya}
-\\newfontfamily\\odiafont[Script=Oriya]{Noto Sans Oriya}
-
-\\begin{document}
-
-Some English text here.
-
-\\begin{oriya}
-ଏହା ଗୋଟିଏ ଓଡ଼ିଆ ବାକ୍ୟ |
-\\end{oriya}
-
-\\end{document}`;
-
 const ForAuthors: React.FC = () => {
   const { t } = useTranslation();
 
   return (
     <div className="page-container authors-page">
-      <h1>{t("forAuthors.title")}</h1>
 
-      <p className="lead">{t("forAuthors.intro")}</p>
+      <section className="section contact">
+        <h3>{t("forAuthors.submissionTitle")}</h3>
+        <p>{t("forAuthors.submissionInstruction")}</p>
+      </section>
 
       <section className="section">
         <h2>{t("forAuthors.scopeTitle")}</h2>
@@ -44,56 +28,22 @@ const ForAuthors: React.FC = () => {
         <p>{t("forAuthors.latexIntro")}</p>
         <p>
           <a
-            href="/styles/leelavati.sty"
+            href="https://github.com/leelavati-ocmse/latex"
             className="btn-link"
             title="Download Leelavati LaTeX style file"
           >
-            {t("forAuthors.downloadSty")}
-          </a>
-          &nbsp;&nbsp;({t("forAuthors.styNote")})
-        </p>
-
-        <h3>{t("forAuthors.quickInstructionsTitle")}</h3>
-        <ol className="guidelines">
-          <li>{t("forAuthors.quickInstructions.step1")}</li>
-          <li>{t("forAuthors.quickInstructions.step2")}</li>
-          <li>{t("forAuthors.quickInstructions.step3")}</li>
-        </ol>
+            Download leelavati style file folder
+          </a> </p>
+         <p>{t("forAuthors.styNote")}</p>
       </section>
 
       <section className="section">
-        <h2>{t("forAuthors.odiaTitle")}</h2>
-        <p>{t("forAuthors.odiaIntro")}</p>
-
-        <p className="example-title">{t("forAuthors.odiaExampleTitle")}</p>
-
-        <pre className="code-block">{latexExample}</pre>
-
-        <p>{t("forAuthors.notesTitle")}</p>
-        <ul className="guidelines">
-          <li>{t("forAuthors.notes.fontInstall")}</li>
-          <li>{t("forAuthors.notes.compile")}</li>
-          <li>{t("forAuthors.notes.fontIssues")}</li>
-        </ul>
-
-        <p>{t("forAuthors.advancedSetup")}</p>
-
+       <p> {t("forAuthors.odiaTypeIntro")} </p>
         <p>
           <a href="/odia_typing/index.html" className="btn-secondary">
-            {t("forAuthors.odiaDemo")}
+            Odia Typing Tool
           </a>
         </p>
-      </section>
-
-      <section className="section contact">
-        <h3>{t("forAuthors.submissionTitle")}</h3>
-        <p
-          dangerouslySetInnerHTML={{
-            __html: t("forAuthors.submissionDetails", {
-              email: "submit@edumag.org",
-            }),
-          }}
-        />
       </section>
     </div>
   );
